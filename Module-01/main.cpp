d999a4211b88806294b3ffb90f36477b1ac68256
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-10 06:59:52 -0600

    Updating Module 01

--- Module-01/main.cpp
+++ Module-01/main.cpp
@@ -6,6 +6,10 @@
 
 #include <iostream>
 
+float addition(float num1, float num2);
+float division(float num1, float num2);
+void print_result(std::string result_type, float result_value);
+
 int main() {
 
 	float first_no, second_no; 
@@ -16,12 +20,29 @@ int main() {
 	std::cout << "Enter second number\t";
 	std::cin >> second_no;
 
-	result_add = first_no + second_no;
-	result_div = first_no / second_no;
+	result_add = addition(first_no, second_no);
+	result_div = division(first_no, second_no);
 
-	std::cout << "Addition result:\t" << result_add 
+	/* std::cout << "Addition result:\t" << result_add 
 		  << "\nDivision result:\t" << result_div 
 		  << "\n";
+	*/
+
+	print_result("Addition: ", result_add);
+	print_result("Division: ", result_div);
 
 	return 0;
+}
+
+float addition(float num1, float num2) {
+	return num1 + num2;	
+}
+
+float division(float num1, float num2) {
+	return num1 / num2;
+}
+
+void print_result(std::string result_type, float result_value) {
+	std::cout << result_type << " result:\t" 
+		  << result_value << "\n";
 }
